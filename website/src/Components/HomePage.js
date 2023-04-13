e92793a36e1a7ae2ffa48f77b415b448e5de0a25
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-04-13 16:49:46 -0500

    Optimized the website

--- website/src/Components/HomePage.js
+++ website/src/Components/HomePage.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import SocialMedia from "./SocialMedia";
-import './ComponentCSS/NavAndHomepage.css';
-import smallProfilePic from "../Images/IMG_3877 2.jpg";
-import mediumProfilePic from "../Images/IMG_3877 2.jpg";
+import React, { lazy, Suspense } from 'react';
+import { Link } from 'react-router-dom';
 import largeProfilePic from "../Images/IMG_3877 2.jpg";
+const SocialMedia = lazy(() => import('./SocialMedia'));
 
 export default function HomePage() {
   return (
     <div className="home-page">
-      <picture>
-        <source media="(max-width: 480px)" srcSet={smallProfilePic} />
-        <source media="(max-width: 768px)" srcSet={mediumProfilePic} />
-        <img src={largeProfilePic} alt="Profile" className="home-page-image" />
-      </picture>
+      <img
+        srcSet={`${largeProfilePic} 1024w,
+                  ${largeProfilePic} 768w,
+                  ${largeProfilePic} 480w`}
+        sizes="(max-width: 480px) 100vw,
+               (max-width: 768px) 50vw,
+               33vw"
+        src={largeProfilePic}
+        alt="Profile"
+        className="home-page-image"
+      />
       <h1 className="home-page-title">Christopher Smith Jr</h1>
       <h2 className="home-page-subtitle">New Grad Software Engineer</h2>
       <p className="home-page-description">
         Hi, my name is Christopher Smith Jr and I'm a software engineer with experience in web development.
         I'm passionate about solving problems and creating software that can make a positive impact on people's lives.
       </p>
-      <a href='/About' className="home-page-cta" style={{ textDecoration: 'none' }}>Learn More About Me</a>
-      <SocialMedia/>
+      <Link to='/About' className="home-page-cta" style={{ textDecoration: 'none' }}>Learn More About Me</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <SocialMedia />
+      </Suspense>
     </div>
   );
 }
--- website/src/Pages/Contact.js
+++ website/src/Pages/Contact.js
@@ -1,119 +1,107 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
-import NavBar from "../Components/NavBar";
-import ReCAPTCHA from "react-google-recaptcha";
 import { useState } from "react";
 import emailjs from "emailjs-com";
-import "../Pages/PagesCSS/Contact.css";
+import ReCAPTCHA from "react-google-recaptcha";
+import { Button, Form, FloatingLabel } from "react-bootstrap";
+import NavBar from "../Components/NavBar";
 import SocialMedia from "../Components/SocialMedia";
 import Footer2 from "../Components/Footer2";
+import "../Pages/PagesCSS/Contact.css";
+
+const siteKey = "6Lc7KWwlAAAAADuXR98GHXzgcX8JzcrsIOkw2jEl";
+const serviceId = "service_qkajsi9";
+const templateId = "template_du1ow3i";
+const userId = "uJpx4DXA_gp3IDEks";
 
 function Contact() {
-	const [captchaIsDone, setCaptchaDone] = useState(false);
-	const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-	const [showErrorMessage, setShowErrorMessage] = useState(false);
-	const backgroundColor = "black";
+  const [captchaIsDone, setCaptchaDone] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const backgroundColor = "black";
 
-	function onChange() {
-		console.log("Captcha done");
-		setCaptchaDone(true);
-	}
-
-	function sendEmail(e) {
-		e.preventDefault();
-
+  function sendEmail(e) {
+    e.preventDefault();
     emailjs
-    .sendForm(
-      "service_qkajsi9",
-      "template_du1ow3i",
-      e.target,
-      "uJpx4DXA_gp3IDEks"
-    )
-			.then(
-				(result) => {
-					console.log(result.text);
-					setShowSuccessMessage(true);
-					setShowErrorMessage(false);
-				},
-				(error) => {
-					console.log(error.text);
-					setShowSuccessMessage(false);
-					setShowErrorMessage(true);
-				}
-			);
-		e.target.reset();
-	}
+      .sendForm(serviceId, templateId, e.target, userId)
+      .then(() => {
+        setShowSuccessMessage(true);
+        setShowErrorMessage(false);
+      })
+      .catch(() => {
+        setShowSuccessMessage(false);
+        setShowErrorMessage(true);
+      })
+      .finally(() => e.target.reset());
+  }
 
-	return (
-		<>
-			<NavBar />
-			<div className="container mt-5 mb-5">
-				<div className="row justify-content-center">
-					<div className="col-md-6">
-						<Form onSubmit={sendEmail}>
-							<h1 className="text-center mb-4 slideInTop">Contact Me</h1>
-							<p className="text-center mb-4 slideInTop">
-								Don’t hesitate to reach out with the contact information on my{" "}
-								<a href="/resume" style={{ color: "orange" }}>
-									resume
-								</a>
-								, or send a message using the form
-							</p>
-							<Form.Group controlId="formBasicName">
-								<Form.Label>Name</Form.Label>
-								<Form.Control
-									type="text"
-									name="name"
-									placeholder="Enter your name"
-									required
-								/>
-							</Form.Group>
-							<Form.Group controlId="formBasicEmail">
-								<Form.Label>Email</Form.Label>
-								<Form.Control
-									type="email"
-									name="email"
-									placeholder="Enter your email"
-									required
-								/>
-							</Form.Group>
-							<Form.Group controlId="formBasicSubject">
-								<Form.Label>Subject</Form.Label>
-								<Form.Control
-									type="text"
-									name="subject"
-									placeholder="Enter your subject"
-									required
-								/>
-							</Form.Group>
-							<Form.Group controlId="formBasicMessage">
-								<Form.Label>Message</Form.Label>
-								<FloatingLabel
-									controlId="formBasicMessage"
-									label="Enter your message"
-									className="mb-3"
-								>
-									<Form.Control
-										as="textarea"
-										name="message"
-										style={{ height: "150px" }}
-										required
-									/>
-								</FloatingLabel>
-							</Form.Group>
-							<ReCAPTCHA
-								sitekey="6Lc7KWwlAAAAADuXR98GHXzgcX8JzcrsIOkw2jEl"
-								size="normal"
-								onChange={onChange}
-							/>
-							{showSuccessMessage && (
-								<div className="alert alert-success mt-3" role="alert">
-									Your message has been sent successfully.
-								</div>
-							)}
-							{showErrorMessage && (
-								<div className="alert alert-danger mt-3" role="alert">
+  function handleCaptchaDone() {
+    setCaptchaDone(true);
+  }
+
+  return (
+    <>
+      <NavBar />
+      <div className="container mt-5 mb-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <Form onSubmit={sendEmail}>
+              <h1 className="text-center mb-4 slideInTop">Contact Me</h1>
+              <p className="text-center mb-4 slideInTop">
+                Don’t hesitate to reach out with the contact information on my{" "}
+                <a href="/resume" style={{ color: "orange" }}>
+                  resume
+                </a>
+                , or send a message using the form
+              </p>
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  placeholder="Enter your name"
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicSubject">
+                <Form.Label>Subject</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="subject"
+                  placeholder="Enter your subject"
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicMessage">
+                <Form.Label>Message</Form.Label>
+                <FloatingLabel
+                  controlId="formBasicMessage"
+                  label="Enter your message"
+                  className="mb-3"
+                >
+                  <Form.Control
+                    as="textarea"
+                    name="message"
+                    style={{ height: "150px" }}
+                    required
+                  />
+                </FloatingLabel>
+              </Form.Group>
+              <ReCAPTCHA sitekey={siteKey} size="normal" onChange={handleCaptchaDone} />
+              {showSuccessMessage && (
+                <div className="alert alert-success mt-3" role="alert">
+                  Your message has been sent successfully.
+                </div>
+              )}
+              {showErrorMessage && (
+                <div className="alert alert-danger mt-3" role="alert">
 									Oops! Something went wrong. Please try again later.
 								</div>
 							)}
