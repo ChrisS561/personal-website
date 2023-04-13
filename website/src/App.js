247db25f9ec2fbed47d93eea29282f7f05bb3dec
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-04-13 14:23:48 -0500

    Added Google pageview data

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 import Coverpage from './Pages/Coverpage';
+import gtag from './Components/gtag';
+import ReactGA from "react-ga";
+
 
 function App() {
+ReactGA.initialize("G-GNK4HP7LE8");
+ReactGA.pageview("/");
+ReactGA.pageview("/about");
+ReactGA.pageview("/portfolio");
+ReactGA.pageview("/resume");
+ReactGA.pageview("/contact");
+ReactGA.pageview("/experience");
+
   return (
     <>
     <Coverpage />
